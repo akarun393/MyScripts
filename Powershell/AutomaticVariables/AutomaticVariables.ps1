13a83e5f2643cb3d8e0130e9668998399a532ccf
akarun <akarun393@gmail.com>
2022-10-29 23:33:58 +0530

    AutomaticVariables

---
+++ Powershell/AutomaticVariables/AutomaticVariables.ps1
@@ -0,0 +1,8 @@
+﻿
+#1. $MyInvocation - Determining the directory in which a script is located
+
+If (Test-Path -LiteralPath 'variable:HostInvocation') { $InvocationInfo = $HostInvocation; Write-Host "hostinvocation"} Else { $InvocationInfo = $MyInvocation; Write-Host "myinvocation"}
+	[string]$scriptDirectory = Split-Path -Path $InvocationInfo.MyCommand.Definition -Parent
+
+return $scriptDirectory
+
